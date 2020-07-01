705db51f1daa191ea66de249dcfb817f93e84adf
Rohan Basu <basurohan@gmail.com>
2020-06-30 23:16:14 -0400

    Added data and styling for charts

--- src/App.js
+++ src/App.js
@@ -11,7 +11,7 @@ class App extends Component {
 
   async componentDidMount() {
     const fetchedData = await fetchData();
-    this.setState({ data: fetchData });
+    this.setState({ data: fetchedData });
   }
 
   render() {
